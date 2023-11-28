d000ca43642ca91212efaf3dc7f0bb081f2f53c1
Santiago Garcia <zanty.ago19@gmail.com>
2023-11-28 11:30:07 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Natillera la abuelita toretto
+        //¿ Que necesito para procesar la natillera de 1 cliente?
+        //Entradas
+        int identificadorCliente=3443415;
+        long documentoIdentidad=100063802265L;
+        byte edadCliente=20;
+        short cuetoMensualCliente=25000;
+        float tasaInteresFijo=0.1f;
+        char calificacionCliente='A';
+    }
+}
